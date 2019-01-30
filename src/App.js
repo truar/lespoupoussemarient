912bdfc0f0318e7d60d5faa75310fb7273dd3dc1
truar <thibault.ruaro@gmail.com>
2019-01-30 23:52:28 +0100

    Let's go

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import Wedding from './containers/Wedding';
-import Wait from './containers/Wait';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <Wait />
+      <Wedding />
     );
   }
 }
